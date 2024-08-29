2f3fdcee27db5fa9913ce58016563c021ce9a589
adriel <127214323+adrielmatadordeporco@users.noreply.github.com>
2024-08-29 01:26:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const primeiroNome = "Adriel";
+const sobrenome = "Schwartz";
+const idade = 17;
+const altura = 1.69 + "m";
+const cidadeNatal = "Curitiba";
+const massa = 50 + "Kg";
+
+console.log("O meu primeiro nome é:")
+console.log(primeiroNome);
+console.log("O sobrenome que puxei de minha mãe é:")
+console.log(sobrenome);
+console.log("Minha idade é:")
+console.log(idade);
+console.log("Minha altura é:")
+console.log(altura);
+console.log("Eu peso:")
+console.log(massa);
+console.log("Sou nascido de:")
+console.log(cidadeNatal);
